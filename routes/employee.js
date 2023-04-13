1e9fefb977135616d99c08856792a8f167042229
Aditya Dahake <100963388+dahakeadi15@users.noreply.github.com>
2023-04-14 04:17:55 +0530

    add api routes

--- routes/employee.js
+++ routes/employee.js
@@ -3,9 +3,41 @@ const { getEmployeeByID } = require("../services/employee");
 
 const router = express.Router();
 
-// Get Employee Info
+// Add New Employee -- CREATE
+router.post("/", async (req, res) => {
+  const data = req.body;
+
+  res.json(data);
+});
+
+// Get Employee Info -- READ
 router.get("/:id", async (req, res) => {
-  const data = await getEmployeeByID(req.params.id);
+  const employee_id = req.params.id;
+  const data = employee_id;
+
+  res.json(data);
+});
+
+// List Employees (with Pagination) -- READ
+router.get("/", async (req, res) => {
+  const data = req.body;
+
+  res.json(data);
+});
+
+// Update Employee by id -- UPDATE
+router.put("/:id", async (req, res) => {
+  const employee_id = req.params.id;
+  const data = employee_id;
+
+  res.json(data);
+});
+
+// Delete Employee by id -- DELETE
+router.delete("/:id", async (req, res) => {
+  const employee_id = req.params.id;
+  const data = employee_id;
+
   res.json(data);
 });
 
